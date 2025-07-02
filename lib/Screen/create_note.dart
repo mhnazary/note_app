934db70404b30f3b85bbd5c132027ecccf008218
MHNazary <mhnazary2020@gmail.com>
2025-07-02 11:07:52 +0430

    New note add functionality added

--- lib/Screen/create_note.dart
+++ lib/Screen/create_note.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/model/note_model.dart';
 
 class CreateNote extends StatefulWidget {
-  const CreateNote({super.key});
+  const CreateNote({super.key, required this.onCreateNote});
 
+  final Function(NoteModel) onCreateNote;
   @override
   State<CreateNote> createState() => _CreateNoteState();
 }
 
 class _CreateNoteState extends State<CreateNote> {
+  final titlecontroller = TextEditingController();
+  final storyContoller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Create new Note')),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(children: [TextField(), TextField()]),
+        child: Column(
+          children: [
+            TextField(
+              controller: titlecontroller,
+              decoration: InputDecoration(label: Text('Title')),
+            ),
+            TextField(
+              controller: storyContoller,
+              decoration: InputDecoration(
+                label: Text('story'),
+                border: InputBorder.none,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (titlecontroller.text.isEmpty) return;
+          if (storyContoller.text.isEmpty) return;
+
+          widget.onCreateNote(
+            NoteModel(title: titlecontroller.text, story: storyContoller.text),
+          );
+          Navigator.of(context).pop();
+        },
+        child: Icon(Icons.save),
       ),
     );
   }
--- lib/Screen/home_page.dart
+++ lib/Screen/home_page.dart
@@ -11,13 +11,56 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final List<NoteModel> notes = [];
+
+  void onCreatedNote(NoteModel note) {
+    setState(() {
+      notes.add(note);
+    });
+  }
+
+  void onDeleteNote(int index) {
+    notes.removeAt(index);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Flutter NoteApp')),
       body: ListView.builder(
         itemCount: notes.length,
-        itemBuilder: (cts, index) => Text('data'),
+        itemBuilder:
+            (cts, index) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 8,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        notes[index].title,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(notes[index].story),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (cts) => CreateNote(onCreateNote: onCreatedNote),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
